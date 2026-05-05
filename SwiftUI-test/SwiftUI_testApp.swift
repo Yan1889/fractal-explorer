09e47811c6a92c0bde0796af5f845ec97dc8f3bd
Yan1889 <yan1889v2@gmail.com>
2026-05-05 18:41:50 +0200

    Initial Commit

---
+++ SwiftUI-test/SwiftUI_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_testApp.swift
+//  SwiftUI-test
+//
+//  Created by Yan Amin on 05.05.26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
